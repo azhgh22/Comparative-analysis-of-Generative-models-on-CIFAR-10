2dc59a72577e659d826bf420a129439a89b56b2c
nmach22 <nmach22@freeuni.edu.ge>
2026-01-30 22:54:13 +0400

    add pin memory and numworkers on loader

--- data/cifar10.py
+++ data/cifar10.py
@@ -9,7 +9,9 @@ from torchvision import datasets, transforms
 def load_cifar10(
         batch_size: int=128,
         data_dir: str='./data',
-        normalize_inputs: bool=False) -> tuple[torch.utils.data.DataLoader, torch.utils.data.DataLoader]:
+        normalize_inputs: bool=False,
+        pin_memory: bool=False,
+        num_workers: int=0) -> tuple[torch.utils.data.DataLoader, torch.utils.data.DataLoader]:
     transform_list = [
         transforms.ToTensor(),
         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
@@ -21,7 +23,7 @@ def load_cifar10(
     train_dataset = datasets.CIFAR10(root=data_dir, train=True, download=True, transform=transform)
     test_dataset = datasets.CIFAR10(root=data_dir, train=False, download=True, transform=transform)
     
-    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-    test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True, pin_memory=pin_memory, num_workers=num_workers)
+    test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=False, pin_memory=pin_memory, num_workers=num_workers)
     
     return train_loader, test_loader
